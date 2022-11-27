47a2a881876633856239437fa40a8ff1b5b69ad4
MishaZimuk <mishazimuk@gmail.com>
2022-11-27 19:11:53 +0200

    HW 43

--- multithreading/src/telran/multithreading/Truck.java
+++ multithreading/src/telran/multithreading/Truck.java
@@ -1,36 +1,76 @@
 package telran.multithreading;
 
-public class Truck extends Thread{
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Truck extends Thread {
 	private int load;
 	private int nLoads;
 	private static long elevator1;
 	private static long elevator2;
-	private static final Object mutex = new Object();
+	private static long waitingCounter = 0;
+//	private static final Object mutex = new Object();
+	public static final Lock lock = new ReentrantLock(true);
+
+//	public static final Lock lock1 = new ReentrantLock(true);
+//	public static final Lock lock2 = new ReentrantLock(true);
 	public Truck(int load, int nLoads) {
 		this.load = load;
 		this.nLoads = nLoads;
 	}
+
 	@Override
 	public void run() {
-		for (int i = 0; i < nLoads; i++) {
-			loadElevator1(load);
-			loadElevator2(load);
+		while (!lock.tryLock()) {
+			waitingCounter = getWaitingCounter() + 1;
 		}
-	}
-	synchronized static private void loadElevator2(int load) {
-		 {
-			elevator2 += load;
+		try {
+			for (int i = 0; i < nLoads; i++) {
+				loadElevator1(load);
+				loadElevator2(load);
+			}
+		} finally {
+			lock.unlock();
 		}
-		
+
+//			for (int i = 0; i < nLoads; i++) {
+//				while (!lock1.tryLock()) {
+//					waitingCounter = getWaitingCounter() + 1;
+//				}
+//				try {
+//				loadElevator1(load);
+//				}finally {
+//					lock1.unlock();
+//				}
+//				while (!lock2.tryLock()) {
+//					waitingCounter = getWaitingCounter() + 1;
+//				}
+//				try {
+//				loadElevator2(load);
+//				}finally {
+//					lock2.unlock();
+//				}
+//			}
+
 	}
-	synchronized static private void loadElevator1(int load) {
+
+	static private void loadElevator2(int load) {
+		elevator2 += load;
+	}
+
+	static private void loadElevator1(int load) {
 		elevator1 += load;
-		
 	}
+
 	public static long getElevator1() {
 		return elevator1;
 	}
+
 	public static long getElevator2() {
 		return elevator2;
+	}
+
+	public static long getWaitingCounter() {
+		return waitingCounter;
 	}
 }
